21cbe26dc9818345fbb6448248cf005819135f19
Justine Paul Alvaro <jppalvaro@gmail.com>
2024-04-30 16:19:03 +0800

    Chore: Add & Setup MaterialUI package
    - Add MUI package & dependencies
    - Setup theme provider in app root to utilize MUI functionality
    
    Other changes:
    - Update react dependency version to align with MUI installation guide

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createTheme } from '@mui/material';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from '@mui/styles';
+
+const theme = createTheme();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
